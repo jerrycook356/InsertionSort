7e6c07b37addf075e5138736b45e5c8cf4455bb7
jerrycook356 <Jerrycook356@gmail.com>
2018-08-16 11:57:13 -0400

    first commit

---
+++ Insertion/InsertionSort.h
@@ -0,0 +1,15 @@
+/**@file InsertionSort.h*/
+#pragma once
+#ifndef _INSERTION_SORT
+#define _INSERTION_SORT
+
+template <class ItemType>
+class InsertionSort
+{
+public:
+	void insertionSort(ItemType anArray[], int n);
+};
+#include "InsertionSort.cpp"
+#endif // !INSERTION_SORT
+
+
